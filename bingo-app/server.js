e05f6923150dc3b4411885bfc5e59b70a4997458
Jacquelyn <yuencd@gmail.com>
2017-03-31 11:15:56 +0800

    sockets working on node side

--- bingo-app/server.js
+++ bingo-app/server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var index = require('./routes/index');
 var games = require('./routes/games');
 
-var app = express();
 
 var port = 8888;
 
@@ -27,8 +31,32 @@ app.use('/', index);
 // API
 app.use('/api', games);
 
+//io
+io.on('connection', (socket)=> {
+  console.log('The user is connected');
+});
+  // console.log('new connection made');
+
+  // socket.on('event1', (data) => {
+  //   console.log(data.msg);
+  // });
+  // socket.on('event2', {
+  //   msg:'Server to client, do you read me? Over.'
+  // });
+  // socket.on('event3', (data)=>{
+  //   console.log(data.msg);
+  //   socket.emit('event4', {
+  //     msg: 'Loud and clear :)'
+  //   });
+  // });
+
+// });
+
+
+
+
 
 //Listen to port
-app.listen(port, function(){
+http.listen(port, function(){
   console.log('server started on port ' + port)
 });
