0cac5f206c98941d90cd2f312f5d775dcd6bea53
Jacquelyn <yuencd@gmail.com>
2017-04-04 16:06:05 +0800

    socket io indexes from client MC routes being sent to bingo board routes

--- bingo-app/public/javascripts/socket-client.js
+++ bingo-app/public/javascripts/socket-client.js
@@ -1,16 +1,20 @@
 $(function () {
   var socket = io.connect('http://localhost:8888');
-    socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { what: 'something' });
+  socket.on('welcome-user', function (data) {
+    console.log(data.msg);
+    socket.emit('thank-server', { msg: 'Happy to be here!' });
   });
 
-
-    $('.bingo-questions').each(function () {
-        var $this = $(this);
-        $this.on("click", function () {
-            console.log($(this).data('questionindex'));
-        });
-    });
+  $('.bingo-questions').each(function() {
+      var $this = $(this);
+      $this.off().on("click", function () {
+          var indexSelected = $(this).data('questionindex');
+          console.log("selected question with index: "+indexSelected);
+          socket.emit('mc-picked-q', { selection: indexSelected});
+      });
+  });
+  socket.on('push-data', function(data){
+    console.log("recieving selected question index: " + data)
+  })
 
 });
--- bingo-app/routes/sockets.js
+++ bingo-app/routes/sockets.js
@@ -2,10 +2,18 @@ module.exports = function( io ) {
   //io
   io.on('connection', function (socket) {
     console.log("connection detected")
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
+    socket.emit('welcome-user', { msg: 'Welcome!' });
+    socket.on('thank-server', function (data) {
+      console.log(data.msg);
+    });
+
+    socket.on('mc-picked-q', function(data){
+      console.log(data.selection);
+      socket.selectedQuestion = data.selection
+      console.log(socket.selectedQuestion)
+      socket.broadcast.emit('push-data', socket.selectedQuestion);
     });
   });
+
 
 }
